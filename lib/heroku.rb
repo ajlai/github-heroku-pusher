b6da3611c7858cfb2c3b429192d0c64cb05f542f
Anthony Lai <anthonylai89@gmail.com>
2011-12-28 04:30:47 -0800

    Fix typo in autologin

--- lib/heroku.rb
+++ lib/heroku.rb
@@ -2,7 +2,7 @@ needs('HEROKU_USERNAME', 'HEROKU_API_KEY')
 
 Heroku::Auth.instance_eval do
   # Autologin
-  credentials = [ENV['HEROKU_USERNAME'], ENV['HEROKU_API_KEY']]
+  @credentials = [ENV['HEROKU_USERNAME'], ENV['HEROKU_API_KEY']]
   # Set up keys if there aren't any
   key_path = "#{home_directory}/.ssh/id_rsa.pub"
   if available_ssh_public_keys.empty?
